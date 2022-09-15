5466ee4f538919770a7763cf7bff44130c8ef524
aidonmaster <canalbpontop@gmail.com>
2022-09-15 14:56:41 -0300

    iniciando o prejeto de carray lib

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main(int argc, char** argv){
+
+    puts("Modelo de teste colaborativo");
+
+    return 0;
+}
