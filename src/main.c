794660a2bb6a6fda92334f89ba8fc189cbf8eb0d
aidonmaster <canalbpontop@gmail.com>
2022-09-15 20:11:59 -0300

    Adicionado

--- src/main.c
+++ src/main.c
@@ -3,25 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 
-typedef struct{
-    int id;
-    char* tag_id;
-}my_data;
-
-my_data getData(){
-}
-// Comentario a partir do original
-void showFunc();
-char charFinder(char);
-char* stringFinder(char*);
-
-// Apenas um comentario
-// Mais um comentario
-
-// 1
-// 2
-// 3
-// 4
 
 int main(int argc, char** argv){
 
